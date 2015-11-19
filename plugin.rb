8465a553e110d024ee17090efe6b5626275130b6
Miguel Ángel <miguelangellopez@realtracksystems.com>
2015-11-19 11:04:20 +0100

    support to profiles

--- plugin.rb
+++ plugin.rb
@@ -11,11 +11,11 @@ module Onebox
     class SteamUserOnebox
       include Engine
 
-      matches_regexp(/^(https?:\/\/)(steamcommunity.com\/id\/)([^\/]+)\/?$/)
+      matches_regexp(/^https?:\/\/steamcommunity.com\/(id|profiles)\/([^\/]+)\/?$/)
 
       def to_html
-        match = @url.match(/\/id\/([^\/]+)/)
-        steamid = match[1]
+        match = @url.match(/\/(id|profiles)\/([^\/]+)/)
+        steamid = match[2]
         "<a href='#{@url}'><img src='http://steamsignature.com/status/spanish/#{steamid}.png' alt='' title='#{steamid}' /></a><a href='steam://friends/add/#{steamid}'><img style='margin-left: -4px;' src='http://steamsignature.com/AddFriend.png'></a>"
       rescue
         @url
